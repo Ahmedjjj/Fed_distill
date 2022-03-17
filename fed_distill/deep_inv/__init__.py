726c9c60e0adaf07952be60ec34fb63e2d6058b4
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-03-17 17:15:19 +0100

    Add top level imports

--- fed_distill/deep_inv/__init__.py
+++ fed_distill/deep_inv/__init__.py
@@ -1,1 +1,5 @@
-from fed_distill.deep_inv.deep_inv import DeepInversion
+from fed_distill.deep_inv.deep_inv import DeepInversion, ResnetCifarDeepInversion
+from fed_distill.deep_inv.loss import DeepInversionLoss, JensonShannonDiv
+from fed_distill.deep_inv.sampler import RandomSampler, TargetSampler
+from fed_distill.deep_inv.student import StudentTrainer
+
