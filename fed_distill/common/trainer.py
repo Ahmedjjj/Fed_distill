217152a0245877938dcdcd97a26a57e18a1aa40e
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-04-03 18:32:12 +0200

    Move BasicTrainer to common.trainer

--- fed_distill/common/trainer.py
+++ fed_distill/common/trainer.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import logging
 from abc import ABC, abstractmethod
 from copy import deepcopy
@@ -5,6 +6,8 @@ from typing import Any, Dict
 
 from fed_distill.common.tester import AccuracyTester
 from torch import nn
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 logger = logging.getLogger(__name__)
 
@@ -21,6 +24,48 @@ class Trainer(ABC):
 
     def train_epoch(self) -> None:
         self.train_for(1)
+
+
+@dataclass
+class BasicTrainer(Trainer):
+    net: nn.Module
+    optimizer: torch.optim.Optimizer
+    scheduler: torch.optim.lr_scheduler._LRScheduler
+    criterion: nn.Module
+    dataset: Dataset
+    batch_size: int
+    device: str = "cuda"
+
+    def __post_init__(
+        self,
+    ) -> None:
+        self.loader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
+        self.train_metrics = {"train_losses": []}
+
+    def _train_batch(self, images, labels) -> float:
+        self.optimizer.zero_grad()
+        prediction = self.net(images.to(self.device))
+        loss = self.criterion(prediction, labels.to(self.device))
+        loss.backward()
+        self.optimizer.step()
+        return float(loss)
+
+    def train_for(self, num_epochs: int = 1) -> None:
+        self.model.train()
+        for _ in range(num_epochs):
+            epoch_loss = 0
+            num_batches = 0
+            for images, labels in self.loader:
+                epoch_loss += self._train_batch(images, labels)
+                num_batches += 1
+            epoch_loss /= num_batches
+            self.train_metrics["train_losses"].append(epoch_loss)
+            self.scheduler.step()
+        return self.train_metrics
+
+    @property
+    def model(self):
+        return self.net
 
 
 class AccuracyTrainer(Trainer):
@@ -57,4 +102,3 @@ class AccuracyTrainer(Trainer):
         final_dict_copy = deepcopy(base_training_dict)
         final_dict_copy["test_accs"] = self.test_accs
         return final_dict_copy
-        
