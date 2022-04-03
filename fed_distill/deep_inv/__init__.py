3d77e270ee2a5f7d47d3f6ca28611e698fe6db39
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-04-03 17:45:05 +0200

    Add useful imports

--- fed_distill/deep_inv/__init__.py
+++ fed_distill/deep_inv/__init__.py
@@ -2,3 +2,4 @@ from fed_distill.deep_inv.deep_inv import DeepInversion
 from fed_distill.deep_inv.loss import DeepInversionLoss, JensonShannonDiv
 from fed_distill.deep_inv.sampler import RandomSampler, TargetSampler
 from fed_distill.deep_inv.student import StudentTrainer
+from fed_distill.deep_inv.growing_dataset import GrowingDataset, DeepInversionGrowingDataset
