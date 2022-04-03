ede0553eba52278b30fc74718ac8c413b1e175c2
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-04-03 18:33:14 +0200

    Helpers

--- fed_distill/cifar10/helpers.py
+++ fed_distill/cifar10/helpers.py
@@ -5,6 +5,7 @@ from torchvision.datasets import CIFAR10
 
 CIFAR10_MEAN, CIFAR10_STD = (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
 
+
 def get_class_name(label: int) -> str:
     class_to_name = {
         0: "aiplane",
@@ -20,9 +21,11 @@ def get_class_name(label: int) -> str:
     }
     return class_to_name[label]
 
+
 def load_cifar10_test(root: str) -> torch.utils.data.Dataset:
     transform = T.Compose([T.ToTensor(), T.Normalize(CIFAR10_MEAN, CIFAR10_STD)])
     return CIFAR10(root=root, train=False, transform=transform)
+
 
 def load_cifar10_train(root: str) -> torch.utils.data.Dataset:
     train_transform = T.Compose(
@@ -36,6 +39,7 @@ def load_cifar10_train(root: str) -> torch.utils.data.Dataset:
     train_dataset = CIFAR10(root, transform=train_transform, download=True)
     return train_dataset
 
+
 def prepare_to_visualize(img: torch.Tensor) -> np.array:
     mean, std = np.array(CIFAR10_MEAN), np.array(CIFAR10_STD)
     denormalize = T.Normalize(tuple(-mean / std), tuple(1.0 / std))
@@ -47,6 +51,4 @@ def prepare_to_visualize(img: torch.Tensor) -> np.array:
         ]
     )
     return transform(img)
-
-
 
