14d0aa8e6bbce9c7c380d509fbe309ac40f75b4a
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-02 18:16:26 +0200

    Rework fed_distill/sampler

--- fed_distill/deep_inv/sampler.py
+++ fed_distill/deep_inv/sampler.py
@@ -1,62 +1,64 @@
-import random
 from abc import ABC, abstractmethod
 from collections import Counter
-from typing import List
+from typing import Dict, Iterable, Iterator, List, Optional, Union
 
 import numpy as np
 import torch
 
 
 class TargetSampler(ABC):
+    def __init__(self, batch_size: int, classes: Union[int, Iterable[int]]) -> None:
+        self.batch_size = batch_size
+        if isinstance(classes, int):
+            self.classes = range(classes)
+        else:
+            self.classes = tuple(set(classes))
+
     @abstractmethod
-    def __iter__(self):
+    def __iter__(self) -> Iterator[torch.Tensor]:
         raise NotImplementedError()
 
 
 class BalancedSampler(TargetSampler):
-    def __init__(self, batch_size: int, num_classes: int) -> None:
-        if not batch_size % num_classes == 0:
+    def __init__(self, batch_size: int, classes: Union[int, Iterable[int]]) -> None:
+        super().__init__(batch_size, classes)
+        if batch_size % len(self.classes) != 0:
             raise ValueError("Batch size has to be a multiple of the number of classes")
-        self.targets = torch.tensor(
-            [[i] * (batch_size // 10) for i in range(num_classes)]
-        ).reshape(-1)
 
-    def __iter__(self):
+        self.__targets = torch.from_numpy(
+            np.repeat(self.classes, batch_size // len(self.classes))
+        )
+
+    def __iter__(self) -> Iterator[torch.Tensor]:
         while True:
-            yield self.targets
-
-
-class RandomSampler(TargetSampler):
-    def __init__(self, batch_size: int, num_classes: int = 10) -> None:
-        self.batch_size = batch_size
-        self.num_classes = num_classes
-
-    def __iter__(self):
-        while True:
-            yield torch.LongTensor(
-                [
-                    random.randint(0, self.num_classes - 1)
-                    for _ in range(self.batch_size)
-                ]
-            )
-
+            yield self.__targets
 
 class WeightedSampler(TargetSampler):
-    def __init__(self, batch_size: int, labels: List[int]) -> None:
-        label_counts = Counter(labels)
-        sorted_counts = [label_counts[k] for k in sorted(label_counts)]
-        self.probabilities = np.array(sorted_counts) / len(labels)
-        self.drawing_labels = len(sorted_counts)
-        self.batch_size = batch_size
-        self.rng = np.random.default_rng(42)
+    def __init__(
+        self,
+        batch_size: int,
+        classes: Union[int, Iterable[int]],
+        p: Optional[Iterable[float]] = None,
+    ) -> None:
+        super().__init__(batch_size, classes)
+        self.p = p
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[torch.Tensor]:
         while True:
             yield torch.from_numpy(
-                self.rng.choice(
-                    self.drawing_labels,
-                    size=self.batch_size,
-                    replace=True,
-                    p=self.probabilities,
-                )
+                np.random.choice(self.classes, self.batch_size, replace=True, p=self.p)
             )
+
+
+class RandomSampler(WeightedSampler):
+    def __init__(self, batch_size: int, classes: Union[int, Iterable[int]]) -> None:
+        super().__init__(batch_size, classes, p=None)
+
+
+def probs_from_labels(labels: Iterable[int]) -> Dict[int, float]:
+    counter = Counter(labels)
+    probs = dict()
+    for k, v in counter.items():
+        probs[k] = v / len(labels)
+    
+    return probs
