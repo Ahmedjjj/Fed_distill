05cf029edad5cc62ce6ece9224f1e1898ba6a1f7
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-03 13:08:51 +0200

    Remove extra imports

--- fed_distill/deep_inv/__init__.py
+++ fed_distill/deep_inv/__init__.py
@@ -1,5 +1,9 @@
 from fed_distill.deep_inv.deep_inv import DeepInversion
 from fed_distill.deep_inv.loss import DeepInversionLoss, JensonShannonDiv
-from fed_distill.deep_inv.sampler import RandomSampler, TargetSampler, WeightedSampler, probs_from_labels
-from fed_distill.deep_inv.student import StudentTrainer
-from fed_distill.deep_inv.growing_dataset import GrowingDataset, DeepInversionGrowingDataset
+from fed_distill.deep_inv.sampler import (
+    RandomSampler,
+    TargetSampler,
+    WeightedSampler,
+    probs_from_labels,
+)
+
