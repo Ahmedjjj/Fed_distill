4f0e400a0d729d319b55998cfab933bc2254b5b3
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-20 17:21:23 +0200

    Add useful import

--- fed_distill/deep_inv/__init__.py
+++ fed_distill/deep_inv/__init__.py
@@ -1,3 +1,7 @@
 from fed_distill.deep_inv.deep_inv import AdaptiveDeepInversion, DeepInversion
 from fed_distill.deep_inv.loss import ADILoss, JensonShannonDiv, DILoss
+from fed_distill.deep_inv.multi_teacher_loss import MultiTeacherADILoss
+from fed_distill.deep_inv.multi_teacher_deep_inv import MultiTeacherDeepInversion
+
+
 
