bc71e7b31cdbd5687b74dba43c7bbdd01dd09b76
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-21 15:14:54 +0200

    Revert "Add useful import"
    This reverts commit 4f0e400a0d729d319b55998cfab933bc2254b5b3.

--- fed_distill/deep_inv/__init__.py
+++ fed_distill/deep_inv/__init__.py
@@ -1,7 +1,3 @@
 from fed_distill.deep_inv.deep_inv import AdaptiveDeepInversion, DeepInversion
 from fed_distill.deep_inv.loss import ADILoss, JensonShannonDiv, DILoss
-from fed_distill.deep_inv.multi_teacher_loss import MultiTeacherADILoss
-from fed_distill.deep_inv.multi_teacher_deep_inv import MultiTeacherDeepInversion
-
-
 
