152a137e50407cdeff5383d253986ac9016d4528
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-30 22:31:43 +0200

    Add documentation

--- fed_distill/cifar10/viz.py
+++ fed_distill/cifar10/viz.py
@@ -6,6 +6,21 @@ from fed_distill.cifar10.constants import CIFAR10_MEAN, CIFAR10_STD
 
 
 def prepare_to_visualize(img: torch.Tensor) -> np.ndarray:
+    """
+    Given an image generated from deep inversion (cifar10),
+    perform the necessary operations in order to visualize it.
+    In particular:
+        . The image need to be denormalized (inverse of whitened) according the cifar10 mean and std
+        . It is brought back into the [0..1] range
+        . Axis 0 and 2 are transposed 
+        . The image is transformed into a numpy array
+
+    Args:
+        img (torch.Tensor): tensor representing an image (shape: (3, 32, 32))
+
+    Returns:
+        np.ndarray: prepared image (shape: (32, 32, 3))
+    """
     mean, std = np.array(CIFAR10_MEAN), np.array(CIFAR10_STD)
     denormalize = T.Normalize(tuple(-mean / std), tuple(1.0 / std))
     transform = T.Compose(
