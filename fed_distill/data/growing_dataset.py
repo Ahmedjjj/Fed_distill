2e8c5bacbed0826e6c73a1b1339637a0397773b1
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-06-30 23:00:24 +0200

    add documentation

--- fed_distill/data/growing_dataset.py
+++ fed_distill/data/growing_dataset.py
@@ -1,3 +1,6 @@
+"""
+This module contains Dataset and Dataloader abstractions that deal with datasets that grow over time
+"""
 import logging
 import math
 import os
@@ -17,6 +20,17 @@ class GrowingDataset(Dataset[Tuple[torch.Tensor, torch.Tensor]]):
         base_dataset: Optional[Dataset[Tuple[torch.Tensor, torch.Tensor]]] = None,
         new_batch_transform: Optional[Callable[[torch.Tensor], torch.Tensor]] = None,
     ) -> None:
+        """
+        Create a GrowingDataset
+
+        Args:
+            stream (Iterator[Tuple[torch.Tensor, torch.Tensor]]): This represents the source from which the dataset will grow,
+            it should return batches of data.
+            base_dataset (Optional[Dataset[Tuple[torch.Tensor, torch.Tensor]]], optional): Underlying dataset (before growing).
+            Defaults to None.
+            new_batch_transform (Optional[Callable[[torch.Tensor], torch.Tensor]], optional): Transform on the new batches 
+            (different from the base dataset). Defaults to None.
+        """
         self.stream = stream
         self.base = base_dataset if base_dataset else dict()
         self.transform = new_batch_transform if new_batch_transform else lambda x: x
@@ -43,6 +57,12 @@ class GrowingDataset(Dataset[Tuple[torch.Tensor, torch.Tensor]]):
         self._labels = torch.cat((self._labels, labels))
 
     def grow(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Add a new batch to the dataset and return it
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: newly generated batch
+        """
         images, labels = next(self.stream)
         images = images.cpu().detach()
         labels = labels.cpu().detach()
@@ -50,9 +70,24 @@ class GrowingDataset(Dataset[Tuple[torch.Tensor, torch.Tensor]]):
         return images, labels
 
     def save(self, path: PathLike) -> None:
+        """
+        Save all previously generated into the given path
+
+        Args:
+            path (PathLike): save location
+        """
         torch.save({"images": self._images, "labels": self._labels}, path)
 
     def load(self, path: PathLike) -> bool:
+        """
+        Load the images from a file, returns True the loading was successful
+
+        Args:
+            path (PathLike): path to load from
+
+        Returns:
+            bool: True if the file was found
+        """
         if os.path.exists(path):
             self._images, self._labels = torch.load(path).values()
             logger.info("Successfully loaded %i images", len(self._labels))
@@ -72,6 +107,19 @@ class GrowingDatasetLoader(DataLoader[Tuple[torch.Tensor, torch.Tensor]]):
         shuffle: bool = True,
         **loader_kwargs
     ):
+        """
+        A dataloader that automatically grows a dataset each time its __iter__ method
+        is called
+
+        Args:
+            dataset (GrowingDataset): Underlying dataset
+            epoch_samples (int): Since the dataset grows, the notion of epoch is not clear,
+            this parameter allows to set a number of samples that are seen in one epoch
+            new_batches_per_epoch (int, optional): The number of batches to generate in one epoch.
+            Defaults to 2.
+            batch_size (Optional[int], optional): Batch size in each iteration. Defaults to 1.
+            shuffle (bool, optional): If true, the dataset in randomnly shuffled in each iteration. Defaults to True.
+        """
         super().__init__(dataset, batch_size, shuffle, **loader_kwargs)
         self._epoch_num_batches = math.floor(epoch_samples / batch_size)
         logger.info("Number of batches per epoch: %i", self._epoch_num_batches)
