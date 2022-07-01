762d8014d70a7a11fc4100d1e6adb16d0707d7c2
ahmedjjj <ahmed.jellouli@epfl.ch>
2022-07-01 16:50:26 +0200

    Documentation

--- fed_distill/data/iterator.py
+++ fed_distill/data/iterator.py
@@ -8,6 +8,15 @@ logger = logging.getLogger(__name__)
 def mix_iterators(
     iterators: Sequence[Iterator[Tuple[torch.Tensor, torch.Tensor]]]
 ) -> Iterator[Tuple[torch.Tensor, torch.Tensor]]:
+    """
+    Given a sequence of iterators, cycle through them and generate one element from each iteratively
+
+    Args:
+        iterators (Sequence[Iterator[Tuple[torch.Tensor, torch.Tensor]]]): input iterators
+
+    Yields:
+        Iterator[Tuple[torch.Tensor, torch.Tensor]]: "mixed" iterator
+    """
     i = 0
     while True:
         logger.info("Generating batch from teacher %i", i)
--- fed_distill/data/label_sampler.py
+++ fed_distill/data/label_sampler.py
@@ -1,3 +1,6 @@
+"""
+This module contains classes that endlessly yield tensors of labels, used for choosing batches for deep inversion
+"""
 from abc import ABC, abstractmethod
 from collections import Counter
 from typing import Dict, Iterable, Iterator, List, Optional, Union
@@ -21,6 +24,15 @@ class TargetSampler(ABC):
 
 class BalancedSampler(TargetSampler):
     def __init__(self, batch_size: int, classes: Union[int, Iterable[int]]) -> None:
+        """
+        A balanced sampler returns the same amount of targets from each class
+
+        Args:
+            batch_size (int): length of returned vector
+            classes (Union[int, Iterable[int]]): classes to sample from, if int samples from range(classes)
+        Raises:
+            ValueError: if batch_size is not divisible by the number of classes
+        """
         super().__init__(batch_size, classes)
         if batch_size % len(self.classes) != 0:
             raise ValueError("Batch size has to be a multiple of the number of classes")
@@ -41,6 +53,13 @@ class WeightedSampler(TargetSampler):
         classes: Union[int, Iterable[int]],
         p: Optional[Iterable[float]] = None,
     ) -> None:
+        """
+        Sample labels according to a probability distribution
+        Args:
+            batch_size (int): length of returned vector
+            classes (Union[int, Iterable[int]]): classes to sample from, if int samples from range(classes)
+            p (Optional[Iterable[float]], optional): probability over the range of classes. Defaults to None.
+        """
         super().__init__(batch_size, classes)
         self.p = tuple(p) if p else None
 
@@ -52,11 +71,21 @@ class WeightedSampler(TargetSampler):
 
 
 class RandomSampler(WeightedSampler):
+    # Sample uniformly at random
     def __init__(self, batch_size: int, classes: Union[int, Iterable[int]]) -> None:
         super().__init__(batch_size, classes, p=None)
 
 
 def probs_from_labels(labels: Iterable[int]) -> Dict[int, float]:
+    """
+    From a list of labels returns the frequency of each label
+
+    Args:
+        labels (Iterable[int]): input labels
+
+    Returns:
+        Dict[int, float]: Dict of the form {class:frequency}
+    """
     labels = tuple(labels)
     counter = Counter(labels)
     probs = dict()
